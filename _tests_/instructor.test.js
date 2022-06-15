430e6bffb0e6e61f8705f353d117f030bc8bd1c1
rudie-instructor <105744265+rudie-instructor@users.noreply.github.com>
2022-06-14 22:50:44 -0500

    Update instructor.test.js

--- _tests_/instructor.test.js
+++ _tests_/instructor.test.js
@@ -1,6 +1,6 @@
 /* 
     Write tests for an instructor class that returns objects with three properties; name (string), 
-    assistants (empty array) and two methods called addAssistant, which takes a single 
+    assistants (empty array) and a method called addAssistant, which takes a single 
     string argument and alters the assistant property accordingly. Also, write tests that verify that 
     a general error is thrown when the instructor class is provided no argument, and specific errors 
     are thrown when the instructor class and its corresponding method property are provided invalid arguments
